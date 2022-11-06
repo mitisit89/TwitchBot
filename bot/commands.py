31415f2b0ebbfeeb6956b901f0f6073f7e207adc
mitisit89 <mitisit89@gmail.com>
2022-11-06 18:35:57 +0300

    Initial

---
+++ bot/commands.py
@@ -0,0 +1,10 @@
+from twitchio.ext import commands
+
+
+
+
+class Bot(commands.Bot):
+    pass
+
+
+bot=Bot()
---
+++ main.py
@@ -0,0 +1,6 @@
+# subprocess.run(["mpv", 'playlist.m3u','--no-terminal','2&>1 1>/dev/null'],capture_output=False)
+from bot.commands import bot 
+
+
+if __name__ == "__main__":
+    bot.run()
