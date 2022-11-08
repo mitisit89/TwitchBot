59e78bcd37a57ea6e296502dad10dd833196483b
mitisit89 <mitisit89@gmail.com>
2022-11-09 00:09:49 +0300

    work in progress

--- bot/commands.py
+++ bot/commands.py
@@ -1,10 +1,29 @@
 from twitchio.ext import commands
 
-
+from config import TOKEN
 
 
 class Bot(commands.Bot):
-    pass
+    def __init__(self):
+        super().__init__(
+            token=TOKEN,
+            prefix="!",
+            initial_channels=["..."],
+        )
+
+    async def event_ready(self):
+        print(f"Logged in as | {self.nick}")
+        print(f"User id is | {self.user_id}")
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        # Send a hello back!
+        await ctx.send(f"Hello {ctx.author.name}!")
+
+    @commands.command()
+    async def sr(self, ctx: commands.Context):
+        # Command for song request
+        pass
 
 
-bot=Bot()
+bot = Bot()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # subprocess.run(["mpv", 'playlist.m3u','--no-terminal','2&>1 1>/dev/null'],capture_output=False)
-from bot.commands import bot 
-
+from bot import bot
 
 if __name__ == "__main__":
     bot.run()
